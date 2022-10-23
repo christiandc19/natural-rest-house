9accf9594105f7745d89ec685df707e07dcdbdb7
Chris <christiandc19@Chriss-MBP.lan>
2022-10-22 22:04:46 -0700

    initial files created

---
+++ src/components/mental-health/Anxiety.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Anxiety = () => {
+       return (
+              <div className="sec__one">
+                     <h1> Anxiety </h1>
+              </div>
+       )
+}
+
+export default Anxiety
---
+++ src/components/menu/Navbarmenu.js
@@ -0,0 +1,127 @@
+import React,{useState} from 'react';
+import {NavLink, Link} from 'react-router-dom';
+import {FiAlignRight,FiXCircle,FiChevronDown } from "react-icons/fi";
+import logo from '../../img/logo.png';
+
+
+const Navbarmenu = () => {
+
+    const [isMenu, setisMenu] = useState(false);
+    const [isResponsiveclose, setResponsiveclose] = useState(false);
+    const toggleClass = () => {
+      setisMenu(isMenu === false ? true : false);
+      setResponsiveclose(isResponsiveclose === false ? true : false);
+  };
+
+    let boxClass = ["main-menu menu-right menuq1"];
+    if(isMenu) {
+        boxClass.push('menuq2');
+    }else{
+        boxClass.push('');
+    }
+
+    const [isMenuSubMenu, setMenuSubMenu] = useState(false);
+      
+    const toggleSubmenu = () => {
+      setMenuSubMenu(isMenuSubMenu === false ? true : false);
+    };
+    
+    let boxClassSubMenu = ["sub__menus"];
+    if(isMenuSubMenu) {
+        boxClassSubMenu.push('sub__menus__Active');
+    }else {
+        boxClassSubMenu.push('');
+    }
+
+
+
+    const [isMenuSubMenu2, setMenuSubMenu2] = useState(false);
+      
+    const toggleSubmenu2 = () => {
+      setMenuSubMenu2(isMenuSubMenu2 === false ? true : false);
+    };
+    
+    let boxClassSubMenu2 = ["sub__menus"];
+    if(isMenuSubMenu2) {
+        boxClassSubMenu2.push('sub__menus__Active');
+    }else {
+        boxClassSubMenu2.push('');
+    }
+   
+
+    return (
+    <header className="header__middle">
+        <div className="container">
+            <div className="row">
+
+                {/* Add Logo  */}
+                <div className="header__middle__logo">
+                    <NavLink exact activeClassName='is-active' to="/">
+                        <img src={logo} alt="logo" /> 
+                    </NavLink>
+                </div>
+
+                <div className="header__middle__menus">
+                    <nav className="main-nav " >
+
+                    {/* Responsive Menu Button */}
+                    {isResponsiveclose === true ? <> 
+                        <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiXCircle />   </span>
+                    </> : <> 
+                        <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiAlignRight />   </span>
+                    </>}
+
+
+                    <ul className={boxClass.join(' ')}>
+
+                    {/* HOME */}
+                    <li  className="menu-item" >
+                        <NavLink exact activeClassName='is-active' onClick={toggleClass} to={`/`}> Home </NavLink> 
+                    </li>
+
+
+                    {/* MENTAL HEALTH */}
+                    <li onClick={toggleSubmenu} className="menu-item sub__menus__arrows" > <Link to="#"> Mental Health <FiChevronDown /> </Link>
+                        <ul className={boxClassSubMenu.join(' ')} > 
+                            <li> <NavLink onClick={toggleClass} activeClassName='is-active'  to={`/Online`}> Anxiety </NavLink> </li>
+                            <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/Offline`}> Bipolar </NavLink> </li>
+                        </ul>
+                    </li>
+
+                    {/* ABOUTE */}
+                    <li className="menu-item " ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/About`}> About </NavLink> </li>
+
+
+                    {/* SHOP */}
+                    <li onClick={toggleSubmenu2} className="menu-item sub__menus__arrows" > <Link to="#"> Shop <FiChevronDown /> </Link>
+                        <ul className={boxClassSubMenu2.join(' ')} > 
+                            <li> <NavLink onClick={toggleClass} activeClassName='is-active'  to={`/Online`}> Online Shop </NavLink> </li>
+                            <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/Offline`}> Offline Shop </NavLink> </li>
+                        </ul>
+                    </li>
+
+
+                    {/* CONTACT */}
+                    <li className="menu-item " ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/Contact`}> Contact </NavLink> </li>
+
+
+
+
+
+                    </ul>
+
+
+                    </nav>     
+                </div>   
+
+
+
+        
+        
+            </div>
+	    </div>
+    </header>
+    )
+}
+
+export default Navbarmenu
