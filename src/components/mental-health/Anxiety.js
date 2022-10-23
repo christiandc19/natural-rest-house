dea55a03f053982c46fef73d1d9f3f97063bc955
Chris <christiandc19@Chriss-MBP.lan>
2022-10-23 07:42:25 -0700

    added section to anxiety page

--- src/components/mental-health/Anxiety.js
+++ src/components/mental-health/Anxiety.js
@@ -7,12 +7,24 @@ const Anxiety = () => {
     <div className='anxiety'>
         <div className='container'>
             <div className='content'>
-                <h1>Natural</h1>
-                <h1>Rest House</h1>
+                <h1>Anxiety</h1>
+                <h1>Disorder Treatment</h1>
                 <div><button>Learn More</button></div>
             </div>
         </div>
     </div>
+
+
+    <div className='anxietySection'>
+        <div className='anxietyContainer'>
+            <div className='anxietyContent'>
+                <h1>Anxiety</h1>
+                <h1>Disorder Treatment</h1>
+                <div><button>Learn More</button></div>
+            </div>
+        </div>
+    </div>
+
 
     </>
   )
